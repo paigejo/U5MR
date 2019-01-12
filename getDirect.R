34dc9271ed610cd1026bb7503a6075e047b04811
Johnny Paige <paigejo@uw.edu>
2019-01-11 18:04:53 -0800

    fixed direct estimate code, added direct estimate scripts for cluster

--- getDirect.R
+++ getDirect.R
@@ -6,7 +6,7 @@ source("neonatalSimStudyWeighted.R")
 # load("simDataMultiBeta-1.75margVar0.0225tausq0gamma-1HHoldVar0urbanOver2.RData")
 # load("simDataMultiBeta-1.75margVar0.0225tausq0.01gamma-1HHoldVar0urbanOver2.RData")
 
-getDirectNaive = function(tausq=0.1^2, test=FALSE) {
+getDirectNaive = function(tausq=0.1^2, test=FALSE, loadResults=FALSE) {
   if(!test)
     load(paste0("simDataMultiBeta-1.75margVar0.0225tausq", round(tausq, 4), "gamma-1HHoldVar0urbanOverSamplefrac0.25.RData"))
   else
@@ -15,83 +15,90 @@ getDirectNaive = function(tausq=0.1^2, test=FALSE) {
   # load("simDataMultiBeta-1.75margVar0.0225tausq0gamma-1HHoldVar0urbanOverSamplefrac0.25Test.RData")
   # load("simDataMultiBeta-1.75margVar0.0225tausq0.01gamma-1HHoldVar0urbanOverSamplefrac0.25Test.RData")
   
-  data4directSRS = list()
-  data4directoverSamp = list()
-  
-  # extend the original simulated datasets to binary format
-  # where each child gets one row in the dataset.
-  for(j in 1:length(SRSDat[[2]])){
-    print(paste0("j: ", j))
-    nChildrenSRS = sapply(SRSDat$clustDat, function(x) {x$numChildren})
-    nChildrenOverSamp = sapply(overSampDat$clustDat, function(x) {x$numChildren})
-    startISRS = 1
-    startIOverSamp = 1
-    for(i in 1:nrow(SRSDat[[2]][[j]])){
-      if(i %% 100 == 1)
-        print(paste0("i: ", i, " / ", nrow(SRSDat[[2]][[j]])))
-      
-      tempSRS = extendData(SRSDat[[2]][[j]][i,], v001 = i)
-      tempOversamp = extendData(overSampDat[[2]][[j]][i,], v001 = i)
-      
-      if(i == 1) {
-        resSRS = data.frame(matrix(nrow = nChildrenSRS[j], ncol=ncol(tempSRS)))
-        resoverSamp = data.frame(matrix(nrow = nChildrenOverSamp[j], ncol=ncol(tempOversamp)))
+  if(!loadResults) {
+    data4directSRS = list()
+    data4directoverSamp = list()
+    
+    # extend the original simulated datasets to binary format
+    # where each child gets one row in the dataset.
+    # for(j in 1:length(SRSDat[[2]])){
+    #   print(paste0("j: ", j))
+    #   nChildrenSRS = sapply(SRSDat$clustDat, function(x) {x$numChildren})
+    #   nChildrenOverSamp = sapply(overSampDat$clustDat, function(x) {x$numChildren})
+    #   startISRS = 1
+    #   startIOverSamp = 1
+    #   for(i in 1:nrow(SRSDat[[2]][[j]])){
+    #     if(i %% 100 == 1)
+    #       print(paste0("i: ", i, " / ", nrow(SRSDat[[2]][[j]])))
+    #     
+    #     tempSRS = extendData(SRSDat[[2]][[j]][i,], v001 = i)
+    #     tempOversamp = extendData(overSampDat[[2]][[j]][i,], v001 = i)
+    #     
+    #     if(i == 1) {
+    #       resSRS = data.frame(matrix(nrow = nChildrenSRS[j], ncol=ncol(tempSRS)))
+    #       resoverSamp = data.frame(matrix(nrow = nChildrenOverSamp[j], ncol=ncol(tempOversamp)))
+    #     }
+    #     resSRS[startISRS:(startISRS - 1 + nrow(tempSRS)),] = tempSRS
+    #     resoverSamp[startIOverSamp:(startIOverSamp - 1 + nrow(tempOversamp)),] = tempOversamp
+    #     
+    #     startISRS = startISRS + nrow(tempSRS)
+    #     startIOverSamp = startIOverSamp + nrow(tempOversamp)
+    #   }
+    #   data4directSRS[[j]] = resSRS
+    #   data4directoverSamp[[j]] = resoverSamp
+    # }
+    
+    
+    for(j in 1:length(SRSDat[[2]])){
+      print(paste0("j: ", j))
+      startISRS = 1
+      startIoverSamp = 1
+      for(i in 1:nrow(SRSDat[[2]][[j]])) {
+        if(i %% 100 == 1)
+          print(paste0("i: ", i))
+
+        tmpSRS = extendData(SRSDat[[2]][[j]][i,], v001 = i)
+        tmpoverSamp = extendData(overSampDat[[2]][[j]][i,], v001 = i)
+
+        # initialize the data frames on the first iteration only
+        if(i == 1) {
+          resSRS = as.data.frame(matrix(nrow=sum(SRSDat[[2]][[j]]$numChildren), ncol=ncol(tmpSRS)))
+          resoverSamp = as.data.frame(matrix(nrow=sum(overSampDat[[2]][[j]]$numChildren), ncol=ncol(tmpoverSamp)))
+        }
+
+        # append to the data frames
+        names(resSRS) = names(tmpSRS)
+        names(resoverSamp) = names(tmpoverSamp)
+        endISRS = startISRS + nrow(tmpSRS) - 1
+        endIoverSamp = startIoverSamp + nrow(tmpoverSamp) - 1
+        resSRS[startISRS:endISRS,] = tmpSRS
+        resoverSamp[startIoverSamp:endIoverSamp,] = tmpoverSamp
+
+        # update row index
+        startISRS = endISRS + 1
+        startIoverSamp = endIoverSamp + 1
       }
-      resSRS[startISRS:(startISRS - 1 + nrow(tempSRS)),] = tempSRS
-      resoverSamp[startIOverSamp:(startIOverSamp - 1 + nrow(tempOversamp)),] = tempOversamp
-      
-      startISRS = startISRS + nrow(tempSRS)
-      startIOverSamp = startIOverSamp + nrow(tempOversamp)
+      if(any(is.na(resSRS)) || any(is.na(resoverSamp)))
+        stop()
+      data4directSRS[[j]] = resSRS
+      data4directoverSamp[[j]] = resoverSamp
     }
-    data4directSRS[[j]] = resSRS
-    data4directoverSamp[[j]] = resoverSamp
+    
+    tc = ifelse(tausq == 0, "", paste0("Tausq", round(tausq, 4)))
+    if(!test)
+      save(data4directSRS, data4directoverSamp, file=paste0("data4direct", tc, ".RData"))
+    else
+      save(data4directSRS, data4directoverSamp, file=paste0("data4direct", tc, "Test.RData"))
+    # save(data4directSRS, data4directoverSamp, file="data4directTausq0.01.RData")
+    # save(data4directSRS, data4directoverSamp, file="data4directTest.RData")
+    # save(data4directSRS, data4directoverSamp, file="data4directTausq0.01Test.RData")
+  } else {
+    tc = ifelse(tausq == 0, "", paste0("Tausq", round(tausq, 4)))
+    if(!test)
+      load(paste0("data4direct", tc, ".RData"))
+    else
+      load(paste0("data4direct", tc, "Test.RData"))
   }
-  
-  
-  for(j in 1:length(SRSDat[[2]])){
-    print(paste0("j: ", j))
-    startISRS = 1
-    startIoverSamp = 1
-    for(i in 1:nrow(SRSDat[[2]][[j]])) {
-      if(i %% 100 == 1)
-        print(paste0("i: ", i))
-      
-      tmpSRS = extendData(SRSDat[[2]][[j]][i,], v001 = i)
-      tmpoverSamp = extendData(overSampDat[[2]][[j]][i,], v001 = i)
-      
-      # initialize the data frames on the first iteration only
-      if(i == 1) {
-        resSRS = as.data.frame(matrix(nrow=sum(SRSDat[[2]][[j]]$numChildren), ncol=ncol(tmpSRS)))
-        resoverSamp = as.data.frame(matrix(nrow=sum(overSampDat[[2]][[j]]$numChildren), ncol=ncol(tmpoverSamp)))
-      }
-      
-      # append to the data frames
-      names(resSRS) = names(tmpSRS)
-      names(resoverSamp) = names(tmpoverSamp)
-      endISRS = startISRS + nrow(tmpSRS) - 1
-      endIoverSamp = startIoverSamp + nrow(tmpoverSamp) - 1
-      resSRS[startISRS:endISRS,] = tmpSRS
-      resoverSamp[startIoverSamp:endIoverSamp,] = tmpoverSamp
-      
-      # update row index
-      startISRS = endISRS + 1
-      startIoverSamp = endIoverSamp + 1
-    }
-    if(any(is.na(resSRS)) || any(is.na(resoverSamp)))
-      stop()
-    data4directSRS[[j]] = resSRS
-    data4directoverSamp[[j]] = resoverSamp
-  }
-  
-  tc = ifelse(tausq == 0, "", paste0("Tausq", round(tausq, 4)))
-  if(!test)
-    save(data4directSRS, data4directoverSamp, file=paste0("data4direct", tc, ".RData"))
-  else
-    save(data4directSRS, data4directoverSamp, file=paste0("data4direct", tc, "Test.RData"))
-  # save(data4directSRS, data4directoverSamp, file="data4directTausq0.01.RData")
-  # save(data4directSRS, data4directoverSamp, file="data4directTest.RData")
-  # save(data4directSRS, data4directoverSamp, file="data4directTausq0.01Test.RData")
-  
   
   # start analysis using naive approach and computing direct estimates
   directEstSRS = naiveSRS = list()
---
+++ runDirect0.01.R
@@ -0,0 +1,3 @@
+source("setup.R")
+source("getDirect.R")
+getDirect(tausq=0.1^2)
