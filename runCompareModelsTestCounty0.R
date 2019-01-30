59affb65a3dbc80635beb611babef58601cd4a6c
Johnny Paige <paigejo@uw.edu>
2019-01-30 10:03:20 -0800

    increased time limit for model comparison scripts

--- runCompareModelsTestCounty0.R
+++ runCompareModelsTestCounty0.R
@@ -1,4 +1,4 @@
 source("setup.R")
 source("compareModels.R")
-runCompareModels2(test=TRUE, tausq=0, resultType="county", sampling="SRS", modelsI=1:7, printIEvery=1, nsim=10, saveResults=TRUE)
+# runCompareModels2(test=TRUE, tausq=0, resultType="county", sampling="SRS", modelsI=1:7, printIEvery=1, nsim=10, saveResults=TRUE)
 runCompareModels2(test=TRUE, tausq=0, resultType="county", sampling="oversamp", modelsI=1:7, printIEvery=1, nsim=10, saveResults=TRUE)
