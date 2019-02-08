1c24c22b25d4f11709c4c3bd7faa734159f2d5bb
Johnny Paige <paigejo@uw.edu>
2019-02-08 13:06:05 -0800

    included debiased BYM models in the model comparison code

--- runCompareModelsCounty0.01.R
+++ runCompareModelsCounty0.01.R
@@ -1,4 +1,4 @@
 source("setup.R")
 source("compareModels.R")
-runCompareModels2(resultType="county", sampling="SRS", modelsI=1:7, printIEvery=1, nsim=10, saveResults=TRUE)
-runCompareModels2(resultType="county", sampling="oversamp", modelsI=1:7, printIEvery=1, nsim=10, saveResults=TRUE)
+runCompareModels2(resultType="county", sampling="SRS", modelsI=1:9, printIEvery=1, nsim=10, saveResults=TRUE)
+runCompareModels2(resultType="county", sampling="oversamp", modelsI=1:9, printIEvery=1, nsim=10, saveResults=TRUE)
