c099ec4ebe749e48e98eb914125c5334967aa906
Johnny Paige <paigejo@uw.edu>
2019-03-06 14:59:02 -0800

    fixed typo

--- runSPDEAllPar.R
+++ runSPDEAllPar.R
@@ -2,4 +2,4 @@ source("setupParallel.R")
 index = as.numeric(commandArgs(trailingOnly = TRUE))
 load("spdeCommandArgs.RData")
 argList = spdeCommandArgs[[index]]
-do.call("resultsSPDEPar", argList)
+do.call("resultsSPDE", argList)
