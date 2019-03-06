0a33acd4831b2fbab13e3829baee992c27175465
Johnny Paige <paigejo@uw.edu>
2019-03-06 15:08:34 -0800

    added large SPDE jobs for more parallelization

---
+++ runSPDEAllParLarge.R
@@ -0,0 +1,5 @@
+source("setupParallelLarge.R")
+index = as.numeric(commandArgs(trailingOnly = TRUE))
+load("spdeCommandArgs.RData")
+argList = spdeCommandArgs[[index]]
+do.call("resultsSPDE", argList)
