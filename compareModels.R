285eb5f6dfb423f46cce83eb937876c107637eea
John Paige <paigejo@gmail.com>
2020-02-26 14:00:36 +0100

    changed scoring rule plotting ranges

--- compareModels.R
+++ compareModels.R
@@ -3552,7 +3552,7 @@ plotCompareModelsAllLocal = function(strictPriors=FALSE, usePrecomputedResults=F
   
   # generate plots for each scoring rule (1-6: bias, variance, mse, crps, coverage, width)
   plotHelper(3, goalVal=0, rangeIncludes=c(0.0488351441837245, 1.96136848911688), scoreName="MSE", plotDHSLegend=FALSE, shareRange=TRUE, logScale=TRUE)
-  plotHelper(1, goalVal=0, rangeIncludes=c(0, -61.4285011791729, 36.4960399713828), scoreName="Bias", plotDHSLegend=TRUE, shareRange=TRUE)
+  plotHelper(1, goalVal=0, rangeIncludes=c(0, -61.4285011791729, 36.9516649422178), scoreName="Bias", plotDHSLegend=TRUE, shareRange=TRUE)
   plotHelper(2, goalVal=0, rangeIncludes=c(0, 17.2193571578031), scoreName="Var", plotDHSLegend=FALSE, shareRange=TRUE)
   plotHelper(4, goalVal=0, rangeIncludes=c(0, 7.64137139664491), scoreName="CRPS", shareRange=TRUE, plotDHSLegend=FALSE)
   plotHelper(5, goalVal=80, rangeIncludes=c(55.1914893617021, 100), scoreName="Cvg", shareRange=TRUE, plotDHSLegend=TRUE)
